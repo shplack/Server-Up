8a58f42bcf05f2fee4dae85bd07cb885dbc81bb1
aleheh201 <aleheh201@studentmail.oru.se>
2022-03-09 17:08:13 +0100

    Added more exception handling

--- src/docker_client.py
+++ src/docker_client.py
@@ -1,39 +1,51 @@
-from docker import from_env
+import sys
+from docker import DockerClient, from_env, errors
 import subprocess
 
-client = from_env()       
-
+client: DockerClient = None
+try:
+    client = from_env()
+except errors.DockerException as de:
+    if 'Permission denied' in str(de):
+        print('Do you have permission to use docker?')
+    elif 'Connection refused' in str(de):
+        print('Do you have docker installed?')
+    print(str(de))
+    sys.exit(-1)
 
 def _start_stop(process: str, args: list, timeout: int, log: callable) -> bool:
     log(f'{process.lower().capitalize()}ing containers...')
     
-    proc = subprocess.Popen(
-        args=args,
-        text=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
     success = False
+    
     try:
-        proc.wait(timeout)
-        success = True
-    except subprocess.TimeoutExpired as te:
-        log(str(te), f'Timeout {process.lower()}ing containers')
-        return False
-    except subprocess.CalledProcessError as cpe:
-        log(str(cpe), 'CalledProcessError')
-        return False
-    except Exception as e:
-        log(str(e), 'Error')
-        return False
-    finally:
-        proc.kill()
-        proc.stderr.close()
-        proc.stdout.close()
+        proc = subprocess.Popen(
+            args=args,
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        
+        try:
+            proc.wait(timeout)
+            success = True
+            log(f'Successfully {process.lower()}ed containers')
+        except subprocess.TimeoutExpired as te:
+            log(str(te), f'Timeout {process.lower()}ing containers')
+        except subprocess.CalledProcessError as cpe:
+            log(str(cpe), 'CalledProcessError')
+        except Exception as e:
+            log(str(e), 'Error')
+        finally:
+            proc.kill()
+            proc.stderr.close()
+            proc.stdout.close()
+        
+    except FileNotFoundError as fnfe:
+        log(str(fnfe), 'FileNotFoundError')
+        print("Is docker-compose installed and on path?")
+        
     
-    
-    if success:
-        log(f'Successfully {process.lower()}ed containers')
     return success
 
 def remove_containers(dockerfile: str, docker_timeout: int, log=lambda x: x) -> bool:
