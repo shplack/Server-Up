a11bb164145b831b515765d80e645af1ee708702
aleheh201 <aleheh201@studentmail.oru.se>
2022-03-10 20:06:33 +0100

    forgot to change default ping offline count

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ dockerfile = _get_config('DOCKERFILE')
 docker_timeout = int(_get_config('DOCKER_TIMEOUT') or 60)
 host = _get_config('HOST')
 p_on_count = _get_config('PING_ONLINE_COUNT') or '16'
-p_off_count = _get_config('PING_OFFLINE_COUNT') or '128'
+p_off_count = _get_config('PING_OFFLINE_COUNT') or '64'
 
 
 if not mountDir:
